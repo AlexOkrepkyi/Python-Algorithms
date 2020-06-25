9b1fa8e9f40f4419c7a5ab659a41850a47aa7388
Alex Okrepkyi <creating.smth@gmail.com>
2020-06-25 16:43:20 +0300

    refactored return row

--- Leetcode/Two Sum.py
+++ Leetcode/Two Sum.py
@@ -6,7 +6,7 @@ class Solution:
 
         for i, num in enumerate(nums):
             if target - num in d:             # check, whether second number is in dictionary (has already be found previously)
-                return (d[target - num], i)   # if so, return its index along with index of the current number
+                return d[target - num], i   # if so, return its index along with index of the current number
             d[num] = i                        # if not, add current number and its index to the dictionary
 
 """
